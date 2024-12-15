96dbbe8d8231744d470cd2678fceeae37f200254
Pgas <shantaram.ff@gmail.com>
2024-12-15 17:23:08 -0300

    falta links, textos y movil, almacenar contactos

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+// Usamos `type` en lugar de `interface`
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 export const Input: React.FC<InputProps> = ({ className = '', ...props }) => {
   return (
     <input
-      className={`px-4 py-2 rounded border border-gray-700 bg-gray-800 text-gray-100 focus:ring-2 focus:ring-blue-500 ${className}`}
+      className={`px-4 py-2 rounded border border-gray-300 focus:ring-2 focus:ring-blue-500 ${className}`}
       {...props}
     />
   )
--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+// Usamos `type` en lugar de `interface`
+type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 export const Textarea: React.FC<TextareaProps> = ({ className = '', ...props }) => {
   return (
     <textarea
-      className={`px-4 py-2 rounded border border-gray-700 bg-gray-800 text-gray-100 focus:ring-2 focus:ring-blue-500 ${className}`}
+      className={`px-4 py-2 rounded border border-gray-300 focus:ring-2 focus:ring-blue-500 ${className}`}
       {...props}
     />
   )
